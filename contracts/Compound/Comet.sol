75a4ee1a4f3e70d0a2ca0669d600b886c718b282
Matin Kaboli <matinkaboli@aol.com>
2023-01-27 14:05:32 +0330

    fix: move IWETH9 to interfaces/

--- contracts/Compound/Comet.sol
+++ contracts/Compound/Comet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../IWETH9.sol";
+import "../interfaces/IWETH9.sol";
 import "../interfaces/Permit2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
