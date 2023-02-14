ef5924d5a41c65b21374d8cfe734caaf78fd51dc
Matin Kaboli <matinkaboli@aol.com>
2023-02-14 12:53:08 +0330

    chore: change solidity version to 0.8.18

--- contracts/interfaces/Permit2.sol
+++ contracts/interfaces/Permit2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.18;
 
 import {ISignatureTransfer} from "./ISignatureTransfer.sol";
 import {IAllowanceTransfer} from "./IAllowanceTransfer.sol";
