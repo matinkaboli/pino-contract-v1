41908998254edc6358a2210daa06981a37a91bbb
Matin Kaboli <matinkaboli@aol.com>
2023-04-20 16:46:43 +0330

    docs: add documentations for Lido contract

--- contracts/Lido/Lido.sol
+++ contracts/Lido/Lido.sol
@@ -18,7 +18,8 @@ contract Lido is Proxy {
         weth = _weth;
     }
 
-    function unwrapWETH9() public payable {
+    /// @notice Unwraps WETH to ETH
+    function unwrapWETH9() private {
         uint256 balanceWETH9 = weth.balanceOf(address(this));
 
         if (balanceWETH9 > 0) {
@@ -26,27 +27,38 @@ contract Lido is Proxy {
         }
     }
 
+    /// @notice Sweeps contract tokens to msg.sender
     function sweepToken(IERC20 _token) private {
         uint256 balanceOf = _token.balanceOf(address(this));
 
         _token.safeTransfer(msg.sender, balanceOf);
     }
 
+    /// @notice Sweeps all ST_ETH tokens of the contract based on shares to msg.sender
+    /// @dev This function uses sharesOf instead of balanceOf to transfer 100% of tokens
     function sweepSteth() private {
         uint256 shares = lido.sharesOf(address(this));
 
         lido.transferShares(msg.sender, shares);
     }
 
-    function supply(uint256 _proxyFee) external payable {
-        lido.submit{value: msg.value - _proxyFee}(msg.sender);
+    /// @notice Submits ETH to Lido protocol and transfers ST_ETH to msg.sender
+    /// @param _proxyFee Fee of the proxy contract
+    /// @return steth Amount of ST_ETH token that is being transferred to msg.sender
+    function supply(uint256 _proxyFee) external payable returns (uint256 steth) {
+        steth = lido.submit{value: msg.value - _proxyFee}(msg.sender);
 
         sweepSteth();
     }
 
+    /// @notice Submits WETH to Lido protocol and transfers ST_ETH to msg.sender
+    /// @param _permit Permit2 PermitTransferFrom struct
+    /// @param _signature Signature, used by Permit2
+    /// @return steth Amount of ST_ETH token that is being transferred to msg.sender
     function supplyWeth(ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature)
         external
         payable
+        returns (uint256 steth)
     {
         require(_permit.permitted.token == address(weth));
 
@@ -57,9 +69,10 @@ contract Lido is Proxy {
             _signature
         );
 
+        // Unwrap WETH to ETH
         unwrapWETH9();
 
-        lido.submit{value: _permit.permitted.amount}(msg.sender);
+        steth = lido.submit{value: _permit.permitted.amount}(msg.sender);
 
         sweepSteth();
     }
