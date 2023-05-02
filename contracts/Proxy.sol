a44f99b2462832e2a783454b4ae84d1194ea787b
Matin Kaboli <matinkaboli@aol.com>
2023-05-02 12:29:13 +0330

    refactor: add approveToken and unwrapWETH to proxy contract

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import "./interfaces/Permit2.sol";
+import "./interfaces/IWETH9.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -10,9 +11,11 @@ contract Proxy is Ownable {
 
     error FailedToSendEther();
 
+    IWETH9 public immutable WETH;
     Permit2 public immutable permit2;
 
-    constructor(Permit2 _permit2) {
+    constructor(Permit2 _permit2, IWETH9 _weth) {
+        WETH = _weth;
         permit2 = _permit2;
     }
 
@@ -21,6 +24,36 @@ contract Proxy is Ownable {
         require(address(this).balance > 0);
 
         payable(owner()).transfer(address(this).balance);
+    }
+
+    /// @notice Sweeps contract tokens to msg.sender
+    function sweepToken(IERC20 _token) internal {
+        uint256 balanceOf = _token.balanceOf(address(this));
+
+        _token.safeTransfer(msg.sender, balanceOf);
+    }
+
+    /// @notice Approves an ERC20 token to lendingPool and wethGateway
+    /// @param _token ERC20 token address
+    function approveToken(IERC20 _token, address[] calldata _spenders) external onlyOwner {
+      for (uint8 i = 0; i < _spenders.length;) {
+          _token.safeApprove(_spenders[i], type(uint256).max);
+
+          unchecked {
+            ++i;
+          }
+      }
+    }
+
+    function unwrapWETH9(address recipient) internal {
+        uint256 balanceWETH = WETH.balanceOf(address(this));
+
+        if (balanceWETH > 0) {
+            WETH.withdraw(balanceWETH);
+
+            (bool success,) = recipient.call{value: balanceWETH}("");
+            if (!success) revert FailedToSendEther();
+        }
     }
 
     receive() external payable {}
