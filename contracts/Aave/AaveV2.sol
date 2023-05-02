ed5abe3b59b16c9a792d6f2647c222ebedcc68b4
Matin Kaboli <matinkaboli@aol.com>
2023-05-02 12:31:32 +0330

    refactor: rename LendingPool contreact to AaveV2 and implement proxy contract

--- contracts/Aave/AaveV2.sol
+++ contracts/Aave/AaveV2.sol
@@ -2,91 +2,67 @@
 pragma solidity ^0.8.18;
 
 import "../Proxy.sol";
+import "../interfaces/IWETH9.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface ILendingPool {
     function withdraw(address asset, uint256 amount, address to) external;
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
+        external;
+    function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external;
 }
 
 interface IWethGateway {
     function withdrawETH(address lendingPool, uint256 amount, address to) external;
     function depositETH(address lendingPool, address onBehalfOf, uint16 referralCode) external payable;
+    function repayETH(address lendingPool, uint256 amount, uint256 rateMode, address onBehalfOf) external payable;
+    function borrowETH(address lendingPool, uint256 amount, uint256 interestRateMode, uint16 referralCode) external payable;
 }
 
-/// @title Aave LendingPool proxy contract
+/// @title AaveV2 proxy contract
 /// @author Matin Kaboli
 /// @notice Deposits and Withdraws ERC20 tokens to the lending pool
 /// @dev This contract uses Permit2
-contract LendingPool is Proxy {
+contract AaveV2 is Proxy {
     using SafeERC20 for IERC20;
 
-    address public lendingPool;
-    address public wethGateway;
-    mapping(address => mapping(address => bool)) private alreadyApprovedTokens;
+    ILendingPool public lendingPool;
+    IWethGateway public wethGateway;
 
     /// @notice Sets LendingPool address and approves assets and aTokens to it
     /// @param _lendingPool Aave lending pool address
     /// @param _wethGateway Aave WethGateway contract address
     /// @param _permit2 Address of Permit2 contract
     /// @param _tokens ERC20 tokens, they're approved beforehand
-    /// @param _aTokens underlying ERC20 tokens, they're approved beforehand
-    constructor(
-        address _lendingPool,
-        address _wethGateway,
-        Permit2 _permit2,
-        address[] memory _tokens,
-        address[] memory _aTokens
-    ) Proxy(_permit2) {
+    constructor(Permit2 _permit2, IWETH9 _weth, ILendingPool _lendingPool, IWethGateway _wethGateway, IERC20[] memory _tokens)
+        Proxy(_permit2, _weth)
+    {
         lendingPool = _lendingPool;
         wethGateway = _wethGateway;
 
-        for (uint8 i = 0; i < _tokens.length; ++i) {
-            IERC20(_tokens[i]).safeApprove(_lendingPool, type(uint256).max);
+        for (uint8 i = 0; i < _tokens.length;) {
+            _tokens[i].safeApprove(address(_lendingPool), type(uint256).max);
 
-            alreadyApprovedTokens[_tokens[i]][_lendingPool] = true;
-        }
-
-        for (uint8 i = 0; i < _aTokens.length; ++i) {
-            IERC20(_aTokens[i]).safeApprove(_lendingPool, type(uint256).max);
-
-            alreadyApprovedTokens[_aTokens[i]][_lendingPool] = true;
+            unchecked {
+                ++i;
+            }
         }
     }
 
-    /// @notice Sets LendingPool address and approves assets and aTokens to it
-    /// @param _lendingPool Aave lending pool address
-    function changeLendingPoolAddress(address _lendingPool) public onlyOwner {
+    /// @notice Changes LendingPool and WethGateway address if necessary
+    /// @param _lendingPool Address of the new lending pool contract
+    /// @param _wethGateway Address of the new weth gateway
+    function setNewAddresses(ILendingPool _lendingPool, IWethGateway _wethGateway) external onlyOwner {
         lendingPool = _lendingPool;
-    }
-
-    /// @notice Sets the new WethGateway address
-    /// @param _wethGateway The new WethGateway address
-    function changeWethGatewayAddress(address _wethGateway) public onlyOwner {
         wethGateway = _wethGateway;
-    }
-
-    /// @notice Approves an ERC20 token to lendingPool
-    /// @param _token ERC20 token address
-    function approveToken(address _token) public onlyOwner {
-        IERC20(_token).safeApprove(lendingPool, type(uint256).max);
-
-        alreadyApprovedTokens[_token][lendingPool] = true;
-    }
-
-    /// @notice Approves an ERC20 token to wethGateway
-    /// @param _token ERC20 token address
-    function approveTokenToWethGateway(address _token) public onlyOwner {
-        IERC20(_token).safeApprove(wethGateway, type(uint256).max);
-
-        alreadyApprovedTokens[_token][wethGateway] = true;
     }
 
     /// @notice Deposits an ERC20 token to the pool and sends the underlying aToken to msg.sender
     /// @param _permit Permit2 PermitTransferFrom struct, includes receiver, token and amount
     /// @param _signature Signature, used by Permit2
-    function deposit(ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature)
-        public
+    function supply(ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature)
+        external
         payable
     {
         permit2.permitTransferFrom(
@@ -96,18 +72,16 @@ contract LendingPool is Proxy {
             _signature
         );
 
-        ILendingPool(lendingPool).deposit(_permit.permitted.token, _permit.permitted.amount, msg.sender, 0);
+        lendingPool.deposit(_permit.permitted.token, _permit.permitted.amount, msg.sender, 0);
     }
 
     /// @notice Transfers ETH to WethGateway, then WethGateway converts ETH to WETH and deposits
     /// it to the pool and sends the underlying aToken to msg.sender
-    /// @param _fee Fee of the proxy
-    function depositETH(uint256 _fee) public payable {
-        require(msg.value > 0 && msg.value > _fee);
+    /// @param _proxyFee Fee of the proxy
+    function supplyETH(uint256 _proxyFee) external payable {
+        require(msg.value > _proxyFee);
 
-        uint256 ethValue = msg.value - _fee;
-
-        IWethGateway(wethGateway).depositETH{value: ethValue}(lendingPool, msg.sender, 0);
+        wethGateway.depositETH{value: msg.value - _proxyFee}(address(lendingPool), msg.sender, 0);
     }
 
     /// @notice Receives underlying aToken and sends ERC20 token to msg.sender
@@ -115,7 +89,7 @@ contract LendingPool is Proxy {
     /// @param _signature Signature, used by Permit2
     /// @param _token ERC20 token to receive
     function withdraw(ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature, address _token)
-        public
+        external
         payable
     {
         permit2.permitTransferFrom(
@@ -125,14 +99,14 @@ contract LendingPool is Proxy {
             _signature
         );
 
-        ILendingPool(lendingPool).withdraw(_token, _permit.permitted.amount, msg.sender);
+        lendingPool.withdraw(_token, _permit.permitted.amount, msg.sender);
     }
 
     /// @notice Receives underlying A_WETH and sends ETH token to msg.sender
     /// @param _permit Permit2 PermitTransferFrom struct, includes aToken and amount
     /// @param _signature Signature, used by Permit2
     function withdrawETH(ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature)
-        public
+        external
         payable
     {
         permit2.permitTransferFrom(
@@ -142,6 +116,36 @@ contract LendingPool is Proxy {
             _signature
         );
 
-        IWethGateway(wethGateway).withdrawETH(lendingPool, _permit.permitted.amount, msg.sender);
+        wethGateway.withdrawETH(address(lendingPool), _permit.permitted.amount, msg.sender);
+    }
+
+    function repay(uint8 _rateMode, ISignatureTransfer.PermitTransferFrom calldata _permit, bytes calldata _signature)
+        external
+        payable
+    {
+        permit2.permitTransferFrom(
+            _permit,
+            ISignatureTransfer.SignatureTransferDetails({to: address(this), requestedAmount: _permit.permitted.amount}),
+            msg.sender,
+            _signature
+        );
+
+        lendingPool.repay(_permit.permitted.token, _permit.permitted.amount, _rateMode, msg.sender);
+
+        sweepToken(IERC20(_permit.permitted.token));
+    }
+
+    // The official contract but not gas efficient compared to the other repayETH contract
+    function repayETH(uint256 _rateMode, uint256 _proxyFee) external payable {
+        wethGateway.repayETH{value: msg.value - _proxyFee}(address(lendingPool), msg.value - _proxyFee, _rateMode, msg.sender);
+    }
+
+    function repayETH2(uint256 _rateMode, uint256 _proxyFee) external payable {
+        WETH.deposit{ value: msg.value - _proxyFee}();
+
+        lendingPool.repay(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, msg.value - _proxyFee, _rateMode, msg.sender);
+
+        unwrapWETH9(msg.sender);
     }
 }
+
