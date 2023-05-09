995ab48f44adf820e8cb9d64d0c9e0c19f8adc48
Matin Kaboli <matinkaboli@aol.com>
2023-05-09 18:03:51 +0330

    feat: add aave-v3 protocol and move WethGateway interface to a separate file

--- contracts/Aave/AaveV3.sol
+++ contracts/Aave/AaveV3.sol
@@ -6,37 +6,35 @@ import "./IWethGateway.sol";
 import "../interfaces/IWETH9.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-interface ILendingPool {
+interface AavePoolV3 {
     function withdraw(address asset, uint256 amount, address to) external;
-    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
-        external;
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 refCode) external;
+    function borrow(address asset, uint256 amount, uint256 rateMode, uint16 refCode, address onBehalfOf) external;
     function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external;
 }
 
-/// @title AaveV2 proxy contract
+/// @title AaveV3 proxy contract
 /// @author Matin Kaboli
 /// @notice Deposits and Withdraws ERC20 tokens to the lending pool
 /// @dev This contract uses Permit2
-contract AaveV2 is Proxy {
+contract AaveV3 is Proxy {
     using SafeERC20 for IERC20;
 
-    ILendingPool public lendingPool;
+    AavePoolV3 public pool;
     IWethGateway public wethGateway;
 
     /// @notice Sets LendingPool address and approves assets and aTokens to it
-    /// @param _lendingPool Aave lending pool address
-    /// @param _wethGateway Aave WethGateway contract address
+    /// @param _pool Aave pool address
     /// @param _permit2 Address of Permit2 contract
     /// @param _tokens ERC20 tokens, they're approved beforehand
-    constructor(Permit2 _permit2, IWETH9 _weth, ILendingPool _lendingPool, IWethGateway _wethGateway, IERC20[] memory _tokens)
+    constructor(Permit2 _permit2, IWETH9 _weth, AavePoolV3 _pool, IWethGateway _wethGateway, IERC20[] memory _tokens)
         Proxy(_permit2, _weth)
     {
-        lendingPool = _lendingPool;
+        pool = _pool;
         wethGateway = _wethGateway;
 
         for (uint8 i = 0; i < _tokens.length;) {
-            _tokens[i].safeApprove(address(_lendingPool), type(uint256).max);
+            _tokens[i].safeApprove(address(_pool), type(uint256).max);
 
             unchecked {
                 ++i;
@@ -45,10 +43,10 @@ contract AaveV2 is Proxy {
     }
 
     /// @notice Changes LendingPool and WethGateway address if necessary
-    /// @param _lendingPool Address of the new lending pool contract
+    /// @param _pool Address of the new pool contract
     /// @param _wethGateway Address of the new weth gateway
-    function setNewAddresses(ILendingPool _lendingPool, IWethGateway _wethGateway) external onlyOwner {
-        lendingPool = _lendingPool;
+    function setNewAddresses(AavePoolV3 _pool, IWethGateway _wethGateway) external onlyOwner {
+        pool = _pool;
         wethGateway = _wethGateway;
     }
 
@@ -66,7 +64,7 @@ contract AaveV2 is Proxy {
             _signature
         );
 
-        lendingPool.deposit(_permit.permitted.token, _permit.permitted.amount, msg.sender, 0);
+        pool.supply(_permit.permitted.token, _permit.permitted.amount, msg.sender, 0);
     }
 
     /// @notice Transfers ETH to WethGateway, then WethGateway converts ETH to WETH and deposits
@@ -75,7 +73,7 @@ contract AaveV2 is Proxy {
     function supplyETH(uint256 _proxyFee) external payable {
         require(msg.value > _proxyFee);
 
-        wethGateway.depositETH{value: msg.value - _proxyFee}(address(lendingPool), msg.sender, 0);
+        wethGateway.depositETH{value: msg.value - _proxyFee}(address(pool), msg.sender, 0);
     }
 
     /// @notice Receives underlying aToken and sends ERC20 token to msg.sender
@@ -93,7 +91,7 @@ contract AaveV2 is Proxy {
             _signature
         );
 
-        lendingPool.withdraw(_token, _permit.permitted.amount, msg.sender);
+        pool.withdraw(_token, _permit.permitted.amount, msg.sender);
     }
 
     /// @notice Receives underlying A_WETH and sends ETH token to msg.sender
@@ -110,7 +108,7 @@ contract AaveV2 is Proxy {
             _signature
         );
 
-        wethGateway.withdrawETH(address(lendingPool), _permit.permitted.amount, msg.sender);
+        wethGateway.withdrawETH(address(pool), _permit.permitted.amount, msg.sender);
     }
 
     /// @notice Repays a borrowed token
@@ -128,17 +126,10 @@ contract AaveV2 is Proxy {
             _signature
         );
 
-        lendingPool.repay(_permit.permitted.token, _permit.permitted.amount, _rateMode, msg.sender);
+        pool.repay(_permit.permitted.token, _permit.permitted.amount, _rateMode, msg.sender);
 
         sweepToken(IERC20(_permit.permitted.token));
     }
-
-    ///// @notice Repays ETH
-    ///// @param _rateMode Rate mode, 1 for stable and 2 for variable
-    ///// @param _proxyFee Fee of the proxy contract
-    // function repayETH(uint256 _rateMode, uint256 _proxyFee) external payable {
-    //     wethGateway.repayETH{value: msg.value - _proxyFee}(address(lendingPool), msg.value - _proxyFee, _rateMode, msg.sender);
-    // }
 
     /// @notice Repays ETH using WETH wrap/unwrap
     /// @param _rateMode Rate mode, 1 for stable and 2 for variable
@@ -146,7 +137,7 @@ contract AaveV2 is Proxy {
     function repayETH(uint256 _rateMode, uint256 _proxyFee) external payable {
         WETH.deposit{ value: msg.value - _proxyFee}();
 
-        lendingPool.repay(address(WETH), msg.value - _proxyFee, _rateMode, msg.sender);
+        pool.repay(address(WETH), msg.value - _proxyFee, _rateMode, msg.sender);
 
         unwrapWETH9(msg.sender);
     }
---
+++ test/aave/aave.v3.test.ts
@@ -0,0 +1,622 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { constants } from 'ethers';
+import { PERMIT2_ADDRESS } from '@uniswap/permit2-sdk';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import {
+  loadFixture,
+  time,
+} from '@nomicfoundation/hardhat-network-helpers';
+import {
+  DAI,
+  USDC,
+  USDT,
+  WBTC,
+  WETH,
+  WHALE3POOL,
+} from '../utils/addresses';
+import {
+  AavePoolV3,
+  IERC20,
+  IWETH9,
+  IWethGateway,
+} from '../../typechain-types';
+import { impersonate, signer } from '../utils/helpers';
+
+const POOL = '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2';
+const WETH_GATEWAY = '0xD322A49006FC828F9B5B37Ab215F99B4E5caB19C';
+
+const A_DAI = '0x018008bfb33d285247A21d44E50697654f754e63';
+const A_WETH = '0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8';
+const A_WBTC = '0x5Ee5bf7ae06D1Be5997A1A72006FE6C607eC6DE8';
+const A_USDC = '0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c';
+const A_USDT = '0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a';
+
+describe('Aave - V3', () => {
+  let dai: IERC20;
+  let usdc: IERC20;
+  let usdt: IERC20;
+  let weth: IWETH9;
+  let aDai: IERC20;
+  let aUsdc: IERC20;
+  let aUsdt: IERC20;
+  let aWeth: IERC20;
+  let aave: AavePoolV3;
+  let account: SignerWithAddress;
+  let otherAccount: SignerWithAddress;
+
+  const deploy = async () => {
+    const AaveV3 = await ethers.getContractFactory('AaveV3');
+
+    const contract = await AaveV3.deploy(
+      PERMIT2_ADDRESS,
+      WETH,
+      POOL,
+      WETH_GATEWAY,
+      [USDC, DAI, A_USDC, A_DAI],
+    );
+
+    await contract.approveToken(WETH, [POOL, WETH_GATEWAY]);
+    await contract.approveToken(A_WETH, [POOL, WETH_GATEWAY]);
+
+    return { contract, sign: await signer(account) };
+  };
+
+  before(async () => {
+    const whale = await impersonate(WHALE3POOL);
+    [account, otherAccount] = await ethers.getSigners();
+
+    dai = await ethers.getContractAt('IERC20', DAI);
+    usdc = await ethers.getContractAt('IERC20', USDC);
+    usdt = await ethers.getContractAt('IERC20', USDT);
+    weth = await ethers.getContractAt('IWETH9', WETH);
+    aDai = await ethers.getContractAt('IERC20', A_DAI);
+    aUsdc = await ethers.getContractAt('IERC20', A_USDC);
+    aUsdt = await ethers.getContractAt('IERC20', A_USDT);
+    aWeth = await ethers.getContractAt('IERC20', A_WETH);
+    aave = await ethers.getContractAt('AavePoolV3', POOL);
+
+    const ethAmount = 3n * 10n ** 18n;
+    const daiAmount = 5000n * 10n ** 18n;
+    const usdcAmount = 1500n * 10n ** 6n;
+
+    await usdc.connect(whale).transfer(account.address, usdcAmount);
+    await usdt.connect(whale).transfer(account.address, usdcAmount);
+    await dai.connect(whale).transfer(account.address, daiAmount);
+    await weth.deposit({
+      value: ethAmount,
+    });
+
+    expect(await dai.balanceOf(account.address)).to.gte(daiAmount);
+    expect(await weth.balanceOf(account.address)).to.gte(ethAmount);
+    expect(await usdc.balanceOf(account.address)).to.gte(usdcAmount);
+    expect(await usdt.balanceOf(account.address)).to.gte(usdcAmount);
+
+    await dai.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await weth.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await usdc.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await usdt.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await aDai.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await aUsdc.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await aWeth.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+  });
+
+  describe('Deployment', () => {
+    it('Should deploy with 0 tokens', async () => {
+      const AaveV3 = await ethers.getContractFactory('AaveV3');
+
+      await AaveV3.deploy(
+        PERMIT2_ADDRESS,
+        WETH,
+        POOL,
+        WETH_GATEWAY,
+        [],
+      );
+    });
+
+    it('Should deploy with multiple tokens', async () => {
+      const AaveV3 = await ethers.getContractFactory('AaveV3');
+
+      await AaveV3.deploy(PERMIT2_ADDRESS, WETH, POOL, WETH_GATEWAY, [
+        DAI,
+        USDC,
+        A_DAI,
+        A_USDC,
+      ]);
+    });
+
+    it('Should deploy with all aave tokens and aTokens', async () => {
+      const AaveV3 = await ethers.getContractFactory('AaveV3');
+
+      await AaveV3.deploy(
+        PERMIT2_ADDRESS,
+        WETH,
+        POOL,
+        WETH_GATEWAY,
+        [WBTC, A_WBTC, WETH, A_WETH],
+        {
+          gasLimit: 10_000_000,
+        },
+      );
+    });
+  });
+
+  describe('Supply', () => {
+    it('Should supply USDC', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1000n * 10n ** 6n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      const aUsdcBalance = await aUsdc.balanceOf(account.address);
+
+      await contract.supply(permit, signature);
+
+      expect(await aUsdc.balanceOf(account.address)).to.gt(
+        aUsdcBalance,
+      );
+    });
+
+    it('Should supply USDT', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      await contract.approveToken(USDT, [POOL]);
+
+      const amount = 1000n * 10n ** 6n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: USDT,
+        },
+        contract.address,
+      );
+
+      const aUsdtBalance = await aUsdt.balanceOf(account.address);
+
+      await contract.supply(permit, signature);
+
+      expect(await aUsdt.balanceOf(account.address)).to.gt(
+        aUsdtBalance,
+      );
+    });
+
+    it('Should supply DAI', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1000n * 10n ** 18n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: DAI,
+        },
+        contract.address,
+      );
+
+      const aDaiBalance = await aDai.balanceOf(account.address);
+
+      await contract.supply(permit, signature);
+
+      expect(await aDai.balanceOf(account.address)).to.gt(
+        aDaiBalance,
+      );
+    });
+
+    it('Should supply WETH', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1n * 10n ** 18n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: WETH,
+        },
+        contract.address,
+      );
+
+      const aWethBalance = await aWeth.balanceOf(account.address);
+
+      await contract.supply(permit, signature);
+
+      expect(await aWeth.balanceOf(account.address)).to.gt(
+        aWethBalance,
+      );
+    });
+
+    it('Should supply ETH directly', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const fee = 3000n;
+      const amount = 10n * 10n ** 18n;
+      const minimumAmount = 9n * 10n ** 18n;
+
+      const aWethBalanceBefore = await aWeth.balanceOf(
+        account.address,
+      );
+
+      await contract.supplyETH(fee, {
+        value: amount - fee,
+      });
+
+      const aWethBalanceAfter = await aWeth.balanceOf(
+        account.address,
+      );
+
+      expect(aWethBalanceAfter).to.gte(
+        aWethBalanceBefore.add(minimumAmount),
+      );
+    });
+  });
+
+  describe('Repay', () => {
+    it('Should supply USDC and borrow DAI', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1000n * 10n ** 6n;
+
+      const sign0 = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      await contract.supply(sign0.permit, sign0.signature);
+
+      const borrowAmount = 200n * 10n ** 18n;
+
+      const daiBalanceBefore = await dai.balanceOf(account.address);
+
+      await aave.borrow(DAI, borrowAmount, 2, 0, account.address);
+
+      const daiBalanceAfter = await dai.balanceOf(account.address);
+
+      expect(daiBalanceAfter).to.gt(daiBalanceBefore);
+
+      const sign1 = await sign(
+        {
+          token: DAI,
+          amount: 500n * 10n ** 18n,
+        },
+        contract.address,
+      );
+
+      await contract.repay(2, sign1.permit, sign1.signature);
+    });
+
+    it('Should supply USDC and borrow ETH', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1000n * 10n ** 6n;
+
+      const sign0 = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      await contract.supply(sign0.permit, sign0.signature);
+
+      const borrowAmount = 1n * 10n ** 16n;
+
+      const wethBalanceBefore = await weth.balanceOf(account.address);
+
+      await aave.borrow(WETH, borrowAmount, 2, 0, account.address);
+
+      const wethBalanceAfter = await weth.balanceOf(account.address);
+
+      expect(wethBalanceAfter).to.gt(wethBalanceBefore);
+
+      await contract.repayETH(2, 0, {
+        value: borrowAmount,
+      });
+    });
+
+    it('Should supply USDC and borrow ETH using repayETH2', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1000n * 10n ** 6n;
+
+      const sign0 = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      await contract.supply(sign0.permit, sign0.signature);
+
+      const borrowAmount = 1n * 10n ** 16n;
+
+      const wethBalanceBefore = await weth.balanceOf(account.address);
+
+      // await wethGateway.borrowETH(LENDING_POOL, borrowAmount, 2, 0);
+      await aave.borrow(WETH, borrowAmount, 2, 0, account.address);
+
+      const wethBalanceAfter = await weth.balanceOf(account.address);
+
+      expect(wethBalanceAfter).to.gt(wethBalanceBefore);
+
+      // await contract.approveToken(WETH, [LENDING_POOL]);
+
+      await contract.repayETH(2, 0, {
+        value: borrowAmount * 2n,
+      });
+    });
+  });
+
+  describe('Withdraw', () => {
+    it('Should supply USDC and withdraw it', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 100n * 10n ** 6n;
+      const minimumAmount = 90n * 10n ** 6n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      const aUsdcBalanceBefore = await aUsdc.balanceOf(
+        account.address,
+      );
+
+      await contract.supply(permit, signature);
+
+      const aUsdcBalanceAfter = await aUsdc.balanceOf(
+        account.address,
+      );
+
+      expect(aUsdcBalanceAfter).to.gt(aUsdcBalanceBefore);
+
+      const { signature: signature2, permit: permit2 } = await sign(
+        {
+          token: A_USDC,
+          amount: aUsdcBalanceAfter,
+        },
+        contract.address,
+      );
+
+      const usdcBalanceBefore = await usdc.balanceOf(account.address);
+
+      await contract.withdraw(permit2, signature2, USDC);
+
+      expect(await usdc.balanceOf(account.address)).to.gt(
+        usdcBalanceBefore.add(minimumAmount),
+      );
+    });
+
+    it('Should supply DAI and withdraw it after 1 year', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 100n * 10n ** 18n;
+      const minimumAmount = 101n * 10n ** 18n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: DAI,
+        },
+        contract.address,
+      );
+
+      const aDaiBalanceBefore = await aDai.balanceOf(account.address);
+
+      await contract.supply(permit, signature);
+
+      expect(await aDai.balanceOf(account.address)).to.gt(
+        aDaiBalanceBefore,
+      );
+
+      // Increate the time to 2 years to get some APY
+      const TWO_YEAR_AFTER = 60 * 60 * 24 * 365 * 2;
+      const now = await time.latest();
+      await time.increaseTo(now + TWO_YEAR_AFTER);
+
+      const aDaiBalanceAfter2 = await aDai.balanceOf(account.address);
+
+      const { signature: signature2, permit: permit2 } = await sign(
+        {
+          token: A_DAI,
+          amount: aDaiBalanceAfter2,
+        },
+        contract.address,
+      );
+
+      const daiBalanceBefore = await dai.balanceOf(account.address);
+
+      await contract.withdraw(permit2, signature2, DAI);
+
+      expect(await dai.balanceOf(account.address)).to.gt(
+        daiBalanceBefore.add(minimumAmount),
+      );
+    });
+
+    it('Should supply WETH and withdraw it', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 2n * 10n ** 18n;
+      const minimumAmount = 1n * 10n ** 18n;
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: WETH,
+        },
+        contract.address,
+      );
+
+      const aWethBalanceBefore = await aWeth.balanceOf(
+        account.address,
+      );
+
+      await contract.supply(permit, signature);
+
+      const aWethBalanceAfter = await aWeth.balanceOf(
+        account.address,
+      );
+
+      expect(aWethBalanceAfter).to.gt(aWethBalanceBefore);
+
+      const { signature: signature2, permit: permit2 } = await sign(
+        {
+          amount: aWethBalanceAfter,
+          token: A_WETH,
+        },
+        contract.address,
+      );
+
+      const wethBalanceBefore = await weth.balanceOf(account.address);
+
+      await contract.withdraw(permit2, signature2, WETH);
+
+      expect(await weth.balanceOf(account.address)).to.gt(
+        wethBalanceBefore.add(minimumAmount),
+      );
+    });
+
+    it('Should supply ETH directly and withdraw ETH', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const fee = 3000n;
+      const amount = 10n * 10n ** 18n;
+      const minimumAmount = 9n * 10n ** 18n;
+
+      const aWethBalanceBefore = await aWeth.balanceOf(
+        account.address,
+      );
+
+      await contract.supplyETH(fee, {
+        value: amount - fee,
+      });
+
+      const aWethBalanceAfter = await aWeth.balanceOf(
+        account.address,
+      );
+
+      expect(aWethBalanceAfter).to.gte(
+        aWethBalanceBefore.add(minimumAmount),
+      );
+
+      const balanceBefore = await account.getBalance();
+
+      const { signature, permit } = await sign(
+        {
+          amount: aWethBalanceAfter,
+          token: A_WETH,
+        },
+        contract.address,
+      );
+
+      await contract.withdrawETH(permit, signature);
+
+      expect(await account.getBalance()).to.gt(
+        balanceBefore.add(minimumAmount),
+      );
+    });
+
+    it('Should supply ETH directly and withdraw WETH', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const fee = 3000n;
+      const amount = 10n * 10n ** 18n;
+      const minimumAmount = 9n * 10n ** 18n;
+
+      const aWethBalanceBefore = await aWeth.balanceOf(
+        account.address,
+      );
+
+      await contract.supplyETH(fee, {
+        value: amount - fee,
+      });
+
+      const aWethBalanceAfter = await aWeth.balanceOf(
+        account.address,
+      );
+
+      expect(aWethBalanceAfter).to.gte(
+        aWethBalanceBefore.add(minimumAmount),
+      );
+
+      const wethBalanceBefore = await weth.balanceOf(account.address);
+
+      const { permit, signature } = await sign(
+        {
+          token: A_WETH,
+          amount: aWethBalanceAfter,
+        },
+        contract.address,
+      );
+
+      await contract.withdraw(permit, signature, WETH);
+
+      expect(await weth.balanceOf(account.address)).to.gt(
+        wethBalanceBefore.add(minimumAmount),
+      );
+    });
+  });
+
+  describe('Admin', () => {
+    it('Should change lending pool address', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const newLendingPoolAddress =
+        '0xc6845a5c768bf8d7681249f8927877efda425baf';
+
+      await contract.setNewAddresses(
+        newLendingPoolAddress,
+        WETH_GATEWAY,
+      );
+
+      const currentOwner = await contract.pool();
+
+      expect(currentOwner).to.hexEqual(newLendingPoolAddress);
+    });
+
+    it('Should revert when trying to change lending pool address (not using owner)', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const newLendingPoolAddress =
+        '0xc6845a5c768bf8d7681249f8927877efda425baf';
+
+      await expect(
+        contract
+          .connect(otherAccount)
+          .setNewAddresses(newLendingPoolAddress, WETH_GATEWAY),
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('Should withdraw money', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const amount = 10n * 10n ** 18n;
+
+      await account.sendTransaction({
+        to: contract.address,
+        value: amount,
+      });
+
+      const balanceBefore = await account.getBalance();
+
+      await contract.withdrawAdmin();
+
+      expect(await account.getBalance()).to.gt(balanceBefore);
+    });
+  });
+});
