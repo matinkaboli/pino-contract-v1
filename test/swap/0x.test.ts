b9944cd8f05e4d549b4c3c0c1c29096e8e7498eb
Matin Kaboli <matinkaboli@aol.com>
2023-05-30 15:33:31 +0330

    test: add 0x tests for swaps

---
+++ test/swap/0x.test.ts
@@ -0,0 +1,306 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { constants } from 'ethers';
+import { PERMIT2_ADDRESS } from '@uniswap/permit2-sdk';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import qs from 'qs';
+import {
+  DAI,
+  USDC,
+  USDT,
+  WETH,
+  LUSD,
+  WHALE3POOL,
+} from '../utils/addresses';
+import { IERC20, IWETH9 } from '../../typechain-types';
+import { impersonate, signer } from '../utils/helpers';
+
+const API_QUOTE_URL = 'https://api.0x.org/swap/v1/quote';
+const OneInchV5 = '0x1111111254EEB25477B68fb85Ed929f73A960582';
+const Paraswap = '0x55b916ce078ea594c10a874ba67ecc3d62e29822';
+
+describe('0x', () => {
+  let dai: IERC20;
+  let usdc: IERC20;
+  let usdt: IERC20;
+  let lusd: IERC20;
+  let weth: IWETH9;
+  let account: SignerWithAddress;
+  let otherAccount: SignerWithAddress;
+
+  const deploy = async () => {
+    const SwapAgg = await ethers.getContractFactory(
+      'SwapAggregators',
+    );
+
+    const contract = await SwapAgg.deploy(
+      PERMIT2_ADDRESS,
+      WETH,
+      OneInchV5,
+      Paraswap,
+      [DAI, WETH],
+    );
+
+    return { contract, sign: await signer(account) };
+  };
+
+  before(async () => {
+    const whale = await impersonate(WHALE3POOL);
+    [account, otherAccount] = await ethers.getSigners();
+
+    dai = await ethers.getContractAt('IERC20', DAI);
+    usdc = await ethers.getContractAt('IERC20', USDC);
+    usdt = await ethers.getContractAt('IERC20', USDT);
+    weth = await ethers.getContractAt('IWETH9', WETH);
+    lusd = await ethers.getContractAt('IERC20', LUSD);
+
+    const ethAmount = 3n * 10n ** 18n;
+    const daiAmount = 5000n * 10n ** 18n;
+    const usdcAmount = 1500n * 10n ** 6n;
+
+    await usdc.connect(whale).transfer(account.address, usdcAmount);
+    await usdt.connect(whale).transfer(account.address, usdcAmount);
+    await dai.connect(whale).transfer(account.address, daiAmount);
+    await weth.deposit({
+      value: ethAmount,
+    });
+
+    expect(await dai.balanceOf(account.address)).to.gte(daiAmount);
+    expect(await weth.balanceOf(account.address)).to.gte(ethAmount);
+    expect(await usdc.balanceOf(account.address)).to.gte(usdcAmount);
+    expect(await usdt.balanceOf(account.address)).to.gte(usdcAmount);
+
+    await dai.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await weth.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await usdc.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await usdt.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+    await lusd.approve(PERMIT2_ADDRESS, constants.MaxUint256);
+  });
+
+  describe('Deployment', () => {
+    it('Should deploy with 0 tokens', async () => {
+      const SwapAgg = await ethers.getContractFactory(
+        'SwapAggregators',
+      );
+
+      await SwapAgg.deploy(
+        PERMIT2_ADDRESS,
+        WETH,
+        OneInchV5,
+        Paraswap,
+        [],
+      );
+    });
+
+    it('Should deploy with multiple tokens', async () => {
+      const SwapAgg = await ethers.getContractFactory(
+        'SwapAggregators',
+      );
+
+      await SwapAgg.deploy(
+        PERMIT2_ADDRESS,
+        WETH,
+        OneInchV5,
+        Paraswap,
+        [DAI, USDC],
+      );
+    });
+  });
+
+  describe('Swap', () => {
+    it('Should swap USDC for DAI', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 500n * 10n ** 6n;
+
+      const params = qs.stringify({
+        sellToken: 'USDC',
+        buyToken: 'DAI',
+        sellAmount: amount.toString(),
+      });
+
+      const quote = await fetch(`${API_QUOTE_URL}?${params}`).then(
+        (y) => y.json(),
+      );
+
+      await contract.approveToken(USDC, [quote.allowanceTarget]);
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: USDC,
+        },
+        contract.address,
+      );
+
+      const daiBalanceBefore = await dai.balanceOf(account.address);
+
+      await contract.swap0x(
+        DAI,
+        quote.to,
+        0,
+        permit,
+        signature,
+        quote.data,
+      );
+
+      expect(await dai.balanceOf(account.address)).to.gt(
+        daiBalanceBefore,
+      );
+    });
+
+    it('Should swap USDT for WETH', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 500n * 10n ** 6n;
+
+      const params = qs.stringify({
+        sellToken: 'USDT',
+        buyToken: 'WETH',
+        sellAmount: amount.toString(),
+      });
+
+      const quote = await fetch(`${API_QUOTE_URL}?${params}`).then(
+        (y) => y.json(),
+      );
+
+      await contract.approveToken(USDT, [quote.allowanceTarget]);
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: USDT,
+        },
+        contract.address,
+      );
+
+      const daiBalanceBefore = await weth.balanceOf(account.address);
+
+      await contract.swap0x(
+        WETH,
+        quote.to,
+        0,
+        permit,
+        signature,
+        quote.data,
+      );
+
+      expect(await weth.balanceOf(account.address)).to.gt(
+        daiBalanceBefore,
+      );
+    });
+
+    it('Should swap WETH for USDC', async () => {
+      const { contract, sign } = await loadFixture(deploy);
+
+      const amount = 1n * 10n ** 17n;
+
+      const params = qs.stringify({
+        sellToken: 'WETH',
+        buyToken: 'USDC',
+        sellAmount: amount.toString(),
+      });
+
+      const quote = await fetch(`${API_QUOTE_URL}?${params}`).then(
+        (y) => y.json(),
+      );
+
+      await contract.approveToken(WETH, [quote.allowanceTarget]);
+
+      const { signature, permit } = await sign(
+        {
+          amount,
+          token: WETH,
+        },
+        contract.address,
+      );
+
+      const daiBalanceBefore = await usdc.balanceOf(account.address);
+
+      await contract.swap0x(
+        USDC,
+        quote.to,
+        0,
+        permit,
+        signature,
+        quote.data,
+      );
+
+      expect(await usdc.balanceOf(account.address)).to.gt(
+        daiBalanceBefore,
+      );
+    });
+
+    it('Should swap ETH for LUSD', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const amount = 1n * 10n ** 18n;
+
+      const params = qs.stringify({
+        sellToken: 'ETH',
+        buyToken: LUSD,
+        sellAmount: amount.toString(),
+      });
+
+      const quote = await fetch(`${API_QUOTE_URL}?${params}`).then(
+        (y) => y.json(),
+      );
+
+      const lusdBalanceBefore = await lusd.balanceOf(account.address);
+
+      await contract.swap0xETH(LUSD, quote.to, 0, quote.data, {
+        value: amount,
+      });
+
+      expect(await lusd.balanceOf(account.address)).to.gt(
+        lusdBalanceBefore,
+      );
+    });
+  });
+
+  describe('Admin', () => {
+    it('Should change addresses', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const new1InchAddress =
+        '0xc6845a5c768bf8d7681249f8927877efda425baf';
+
+      await contract.setDexAddresses(new1InchAddress, Paraswap);
+
+      const OneInchAddress = await contract.OInch();
+
+      expect(OneInchAddress).to.hexEqual(new1InchAddress);
+    });
+
+    it('Should revert when trying to change addresses (not using owner)', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const new1InchAddress =
+        '0xc6845a5c768bf8d7681249f8927877efda425baf';
+
+      await expect(
+        contract
+          .connect(otherAccount)
+          .setDexAddresses(new1InchAddress, Paraswap),
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('Should withdraw money', async () => {
+      const { contract } = await loadFixture(deploy);
+
+      const amount = 10n * 10n ** 18n;
+
+      await account.sendTransaction({
+        to: contract.address,
+        value: amount,
+      });
+
+      const balanceBefore = await account.getBalance();
+
+      await contract.withdrawAdmin();
+
+      expect(await account.getBalance()).to.gt(balanceBefore);
+    });
+  });
+});
