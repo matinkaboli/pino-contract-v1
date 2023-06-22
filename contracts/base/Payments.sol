1fbb8a3480c9b8332324cac0c7a097a3246da23f
Matin Kaboli <matinkaboli@aol.com>
2023-06-22 16:21:45 +0430

    fix: add unwrapWETH9 and sweepToken to public functions

--- contracts/base/Payments.sol
+++ contracts/base/Payments.sol
@@ -17,12 +17,13 @@ contract Payments is Errors, Permit {
     constructor(Permit2 _permit2, IWETH9 _weth) Permit(_permit2, _weth) {}
 
     /// @notice Sweeps contract tokens to msg.sender
-    /// @notice _token ERC20 token address
-    function _sweepToken(address _token) internal {
+    /// @param _token ERC20 token address
+    /// @param _recipient The destination address
+    function sweepToken(address _token, address _recipient) public payable {
         uint256 balanceOf = IERC20(_token).balanceOf(address(this));
 
         if (balanceOf > 0) {
-            IERC20(_token).safeTransfer(msg.sender, balanceOf);
+            IERC20(_token).safeTransfer(_recipient, balanceOf);
         }
     }
 
@@ -50,15 +51,17 @@ contract Payments is Errors, Permit {
         _require(success, ErrorCodes.FAILED_TO_SEND_ETHER);
     }
 
-    /// @notice Unwraps WETH9 to Ether and sends the amount to the recipient
-    /// @param _recipient The destination address
-    function _unwrapWETH9(address _recipient) internal {
-        uint256 balanceWETH = WETH.balanceOf(address(this));
 
-        if (balanceWETH > 0) {
-            WETH.withdraw(balanceWETH);
+    /// @notice Approves an ERC20 token to lendingPool and wethGateway
+    /// @param _token ERC20 token address
+    /// @param _spenders ERC20 token address
+    function approveToken(address _token, address[] calldata _spenders) external {
+        for (uint8 i = 0; i < _spenders.length;) {
+            _approve(_token, _spenders[i]);
 
-            _sendETH(_recipient, balanceWETH);
+            unchecked {
+                ++i;
+            }
         }
     }
 
@@ -73,20 +76,45 @@ contract Payments is Errors, Permit {
         _send(address(WETH), _recipient, value);
     }
 
-    /// @notice Receives WETH and unwraps it to ETH and sends to recipient
+    /// @notice Unwraps WETH9 to Ether and sends the amount to the recipient
     /// @param _recipient The destination address
-    /// @param _permit Permit2 PermitTransferFrom struct, includes receiver, token and amount
-    /// @param _signature Signature, used by Permit2
-    function unwrapWETH9(
-        address _recipient,
-        ISignatureTransfer.PermitTransferFrom calldata _permit,
-        bytes calldata _signature
-    ) external payable {
-        _require(_permit.permitted.token == address(WETH), ErrorCodes.TOKENS_MISMATCHED);
+    function _unwrapWETH9(address _recipient) internal {
+        uint256 balanceWETH = WETH.balanceOf(address(this));
 
-        permitTransferFrom(_permit, _signature);
-        WETH.withdraw(_permit.permitted.amount);
+        if (balanceWETH > 0) {
+            WETH.withdraw(balanceWETH);
 
-        _sendETH(_recipient, _permit.permitted.amount);
+            _sendETH(_recipient, balanceWETH);
+        }
     }
+
+    /// @notice Unwraps WETH9 to Ether and sends the amount to the recipient
+    /// @param _recipient The destination address
+    function unwrapWETH9(address _recipient) public payable {
+        uint256 balanceWETH = WETH.balanceOf(address(this));
+
+        if (balanceWETH > 0) {
+            WETH.withdraw(balanceWETH);
+
+            _sendETH(_recipient, balanceWETH);
+        }
+    }
+
+    // /// @notice Receives WETH and unwraps it to ETH and sends to recipient
+    // /// @param _recipient The destination address
+    // /// @param _permit Permit2 PermitTransferFrom struct, includes receiver, token and amount
+    // /// @param _signature Signature, used by Permit2
+    // function unwrapWETH9(
+    //     address _recipient,
+    //     ISignatureTransfer.PermitTransferFrom calldata _permit,
+    //     bytes calldata _signature
+    // ) external payable {
+    //     _require(_permit.permitted.token == address(WETH), ErrorCodes.TOKENS_MISMATCHED);
+    //
+    //     permitTransferFrom(_permit, _signature);
+    //     WETH.withdraw(_permit.permitted.amount);
+    //
+    //     _sendETH(_recipient, _permit.permitted.amount);
+    // }
 }
+
