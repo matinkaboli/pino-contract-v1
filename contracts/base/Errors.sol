c5f12894c1192935207a2925a48286e618d078b2
Matin Kaboli <matinkaboli@aol.com>
2023-07-06 15:15:38 +0430

    fix: make _require internal

--- contracts/base/Errors.sol
+++ contracts/base/Errors.sol
@@ -7,7 +7,7 @@ contract Errors {
     /// @notice Handles custom error codes
     /// @param _condition The condition, if it's false then execution is reverted
     /// @param _code Custom code, listed in Errors.sol
-    function _require(bool _condition, uint256 _code) public pure {
+    function _require(bool _condition, uint256 _code) internal pure {
         if (!_condition) {
             revert ProxyError(_code);
         }
