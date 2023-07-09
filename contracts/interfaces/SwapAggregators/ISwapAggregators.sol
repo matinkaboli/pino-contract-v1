6cb5e65c4a13c4dee3aa2ec0afb16a5c8334f89e
Matin Kaboli <matinkaboli@aol.com>
2023-07-09 15:24:36 +0430

    refactor: remove ETH functions for swap aggregators

--- contracts/interfaces/SwapAggregators/ISwapAggregators.sol
+++ contracts/interfaces/SwapAggregators/ISwapAggregators.sol
@@ -10,30 +10,12 @@ interface ISwapAggregators {
     /// @param _data 1Inch protocol data from API
     function swap1Inch(bytes calldata _data) external payable;
 
-    /// @notice Swaps using 1Inch protocol
-    /// @dev Uses ETH only
-    /// @param _data 1Inch protocol generated data from API
-    /// @param _proxyFee Fee of the proxy contract
-    function swap1InchETH(bytes calldata _data, uint256 _proxyFee) external payable;
-
     /// @notice Swaps using Paraswap protocol
     /// @param _data Paraswap protocol generated data from API
     function swapParaswap(bytes calldata _data) external payable;
-
-    /// @notice Swaps using Paraswap protocol
-    /// @dev Uses ETH only
-    /// @param _data Paraswap protocol generated data from API
-    /// @param _proxyFee Fee of the proxy contract
-    function swapParaswapETH(bytes calldata _data, uint256 _proxyFee) external payable;
 
     /// @notice Swaps using 0x protocol
     /// @param _swapTarget Swap target address, used for sending _data
     /// @param _data 0x protocol generated data from API
     function swap0x(address _swapTarget, bytes calldata _data) external payable;
-
-    /// @notice Swaps using 0x protocol
-    /// @param _swapTarget Swap target address, used for sending _data
-    /// @param _proxyFee Fee of the proxy contract
-    /// @param _data 0x protocol generated data from API
-    function swap0xETH(address _swapTarget, bytes calldata _data, uint24 _proxyFee) external payable;
 }
