5141791b7cd62eef2fac02777e09c3daad44a7e3
Matin Kaboli <matinkaboli@aol.com>
2023-08-03 11:08:36 +0430

    refactor: add compound new contracts

--- contracts/interfaces/Compound/ICEther.sol
+++ contracts/interfaces/Compound/ICEther.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-interface ICToken is IERC20 {
-    function mint(uint256 mintAmount) external returns (uint256);
+interface ICEther is IERC20 {
+    function mint() external payable;
     function redeem(uint256 redeemTokens) external returns (uint256);
     function balanceOfUnderlying(address account) external returns (uint256);
     function underlying() external view returns (address);
-    function repayBorrowBehalf(address borrower, uint256 repayAmount) external returns (uint256);
+    function repayBorrowBehalf(address borrower) external payable returns (uint256);
     function borrow(uint256 borrowAmount) external returns (uint256);
 }
---
+++ contracts/interfaces/Compound/IComptroller.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+interface IComptroller {
+    function enterMarkets(address[] calldata cTokens) external returns (uint256[] memory);
+}
