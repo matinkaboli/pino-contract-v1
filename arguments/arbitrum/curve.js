b18f50d47eeb0ee52aa8b88c6eb87b609cbdcf4c
Matin Kaboli <matinkaboli@aol.com>
2023-08-08 14:09:25 +0430

    fix: remove the space from the address of curve_swap

--- arguments/arbitrum/curve.js
+++ arguments/arbitrum/curve.js
@@ -1,5 +1,5 @@
 const { PERMIT2, arbitrum } = require('../commonAddresses');
 
-const CURVE_SWAP = ' 0x4c2Af2Df2a7E567B5155879720619EA06C5BB15D';
+const CURVE_SWAP = '0x4c2Af2Df2a7E567B5155879720619EA06C5BB15D';
 
 module.exports = [PERMIT2, arbitrum.WETH, CURVE_SWAP];
