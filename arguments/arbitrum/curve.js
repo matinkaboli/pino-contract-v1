71df7defb2001086be35559091e0d226a0baf72d
Matin Kaboli <matinkaboli@aol.com>
2023-08-08 14:05:42 +0430

    fix: change curve_swap address for arbitrum

--- arguments/arbitrum/curve.js
+++ arguments/arbitrum/curve.js
@@ -1,5 +1,5 @@
 const { PERMIT2, arbitrum } = require('../commonAddresses');
 
-const CURVE_SWAP = '0x55b916ce078ea594c10a874ba67ecc3d62e29822';
+const CURVE_SWAP = ' 0x4c2Af2Df2a7E567B5155879720619EA06C5BB15D';
 
 module.exports = [PERMIT2, arbitrum.WETH, CURVE_SWAP];
